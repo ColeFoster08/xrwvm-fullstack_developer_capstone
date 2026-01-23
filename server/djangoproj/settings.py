828d814d3a858fd961672f02ae16912fb82cb35c
colefoster08 <cole.foster@pseudoworld.com>
2026-01-23 00:31:54 -0500

    hopefully done python lint

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,10 +30,18 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://colefoster-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    (
+        'https://colefoster-8000.'
+        'theiadockernext-0-labs-prod-theiak8s-4-tor01.'
+        'proxy.cognitiveclass.ai'
+    )
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://colefoster-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    (
+        'https://colefoster-8000.'
+        'theiadockernext-0-labs-prod-theiak8s-4-tor01.'
+        'proxy.cognitiveclass.ai'
+    )
 ]
 
 REST_FRAMEWORK = {
@@ -99,7 +107,10 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        (
+            'django.contrib.auth.'
+            'password_validation.UserAttributeSimilarityValidator'
+        ),
     },
     {
         'NAME':
